9a94cd4134f30d1e70c9ca4175740e8d9182a76c
hirokimaeda <hirokimaeda22666@gmail.com>
2019-12-10 16:17:08 +0900

    シーン”Title”においてボタンにカーソルを合わせると、アニメーションが発生するようにしました。

---
+++ Test_SatoLab/Assets/Scripts/Title_Start_GameSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_Start_GameSystem : MonoBehaviour
+{
+    //　スタートボタンを押したら実行する
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");//Gameはゲームシーンのスクリプト名に要変更
+    }
+}
