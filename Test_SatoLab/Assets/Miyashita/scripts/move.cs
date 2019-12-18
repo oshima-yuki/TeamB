7bdd492b110836580f983871b710ec5287175029
Hiroby1108 <hirobymiya@gmail.com>
2019-12-18 10:07:02 +0900

    データの移行が完壁ではないが、ある程度移行したもの

---
+++ Test_SatoLab/Assets/Miyashita/scripts/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody rd = this.GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(0.0f,0.0f,1.0f);
+        rd.AddForce(force);
+    }
+}
