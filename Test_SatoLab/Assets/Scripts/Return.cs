6c2eb64f6e5c8a7a97a8cb5dd91874046dbf2ad6
Mochizuki <senri9547hate@gmail.com>
2020-01-07 16:37:46 +0900

    Add:   bakuhatu1.cs →bakuhatu.cs
    CheckPoint script
      CheckPoint.cs
      GameManeger.cs
      PlayerPos.cs
      Return.cs

--- Test_SatoLab/Assets/Scripts/Return.cs
+++ Test_SatoLab/Assets/Scripts/Return.cs
@@ -6,51 +6,105 @@ public class Return : MonoBehaviour
 {
 
     //private readonly int frame = 0;
-    private float time = 0.0f;
+    public float time = 0.0f;
     public GameObject explode;//explodeにはunity上でprefabを関連付けます
-    public string RTagname;
+    //public string RTagname;
+    private GameManeger gm;
 
     //対象のオブジェクト
-    public GameObject desObj;
-    private bool isRespon = false;
-    private Vector3 initPos;
-    private Resporn resporn;
+    private bool[] isRespon = { false, false, false, false, false, false, false, false };
+    public GameObject[] desObjs = new GameObject[8];
+    private Vector3[] initPos = new Vector3[8];
 
     // Use this for initialization 
     void Start()
     {
         //初期値設定 
-        this.initPos = Vector3.zero;
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManeger>();
+        for(int i = 0; i < 8; i++) { this.initPos[i] = gm.lastCheckPointPos[i]; }
+        
     }
     // Update is called once per frame 
     void Update()
     {
-
-        if (isRespon)
+        for (int i = 0; i < 8; i++)
         {
-            time += Time.deltaTime;
-            if (time >= 3.0f)
+            if (isRespon[i])//Playerzzzzz
             {
-                time = 0.0f;
-                isRespon = false;
-                this.desObj.SetActive(true);
-                this.desObj.transform.position = initPos;
+                time += Time.deltaTime;
+                if (time >= 3.0f)
+                {
+                    
+                        time = 0.0f;
+                        isRespon[i] = false;
+                        this.desObjs[i].SetActive(true);
+                        this.desObjs[i].transform.position = gm.lastCheckPointPos[i];
+                    
+                }
             }
+
         }
     }
 
-    void OnCollisionEnter(Collision coll)
+    
+
+    public void OnCollisionEnter(Collision coll)
     {
-        if (coll.gameObject.tag == RTagname)
+        if (coll.gameObject.tag == "Player")//Player
         {
-            this.desObj.SetActive(false);
-            Instantiate(explode, this.desObj.transform.position, Quaternion.identity);
-            isRespon = true;
-            //または
-            //coll.gameObject.SetActive(false);
+            this.desObjs[0].SetActive(false);
+            Instantiate(explode, this.desObjs[0].transform.position, Quaternion.identity);
+            isRespon[0] = true;
         }
 
-       
+        if (coll.gameObject.tag == "Player2")//Player2
+        {
+            this.desObjs[1].SetActive(false);
+            Instantiate(explode, this.desObjs[1].transform.position, Quaternion.identity);
+            isRespon[1] = true;
+        }
+
+        if (coll.gameObject.tag == "Player3")//Player3
+        {
+            this.desObjs[2].SetActive(false);
+            Instantiate(explode, this.desObjs[2].transform.position, Quaternion.identity);
+            isRespon[2] = true;
+        }
+
+        if (coll.gameObject.tag == "Player4")//Player4
+        {
+            this.desObjs[3].SetActive(false);
+            Instantiate(explode, this.desObjs[3].transform.position, Quaternion.identity);
+            isRespon[3] = true;
+        }
+
+        if (coll.gameObject.tag == "Player5")//Player5
+        {
+            this.desObjs[4].SetActive(false);
+            Instantiate(explode, this.desObjs[4].transform.position, Quaternion.identity);
+            isRespon[4] = true;
+        }
+
+        if (coll.gameObject.tag == "Player6")//Player6
+        {
+            this.desObjs[5].SetActive(false);
+            Instantiate(explode, this.desObjs[5].transform.position, Quaternion.identity);
+            isRespon[5] = true;
+        }
+
+        if (coll.gameObject.tag == "Player7")//Player7
+        {
+            this.desObjs[6].SetActive(false);
+            Instantiate(explode, this.desObjs[6].transform.position, Quaternion.identity);
+            isRespon[6] = true;
+        }
+
+        if (coll.gameObject.tag == "Player8")//Player8
+        {
+            this.desObjs[7].SetActive(false);
+            Instantiate(explode, this.desObjs[7].transform.position, Quaternion.identity);
+            isRespon[7] = true;
+        }
     }
 
 }
