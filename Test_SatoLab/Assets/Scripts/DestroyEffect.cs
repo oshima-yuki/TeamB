9cf9c9b8c3cdab7f3b93ee64353a4b84abe09fdc
Hiroby1108 <hirobymiya@gmail.com>
2020-01-15 12:28:04 +0900

    前回やったデータの復元、一回にコース制限を設けた

---
+++ Test_SatoLab/Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour {
+
+    public GameObject explode;
+
+
+	void Update ()
+	{
+
+		if(Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.C))
+		   Destroy(transform.gameObject);
+        Instantiate(explode, this.transform.position, Quaternion.identity);
+	}
+}
