d4373804e84c9cf61c97632b54a1f94db85a083a
hirokimaeda <hirokimaeda22666@gmail.com>
2020-01-21 16:54:22 +0900

    no message

--- Test_SatoLab/Assets/Scripts/move_test.cs
+++ Test_SatoLab/Assets/Scripts/move_test.cs
@@ -1,52 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class move_test : MonoBehaviour
-{
-    Rigidbody rd;
-    Transform myT;
-    float y1, y2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rd = this.GetComponent<Rigidbody>();
-        myT = this.transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            Vector3 force = new Vector3(0.0f, 0.0f, 10.0f);
-            rd.AddForce(force);
-        }
-        else
-        {
-            Vector3 force = new Vector3(0.0f, 0.0f, -1.0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            myT.Rotate(0, y1, 0);
-            if (y1>30)
-            {
-                y1 = 30;
-            }
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            myT.Rotate(0, y2, 0);
-            if (y2>30)
-            {
-                y2 = 30;
-            }
-        }
-
-    }
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -95,4 +47,4 @@ public class move_test : MonoBehaviour
 
     }
 }
->>>>>>> ec2e1f20528ecb8cd334bfc0aad48794af228778
+
